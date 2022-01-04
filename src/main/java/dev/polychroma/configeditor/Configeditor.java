3bc796b3e57da360e846fdffb29c3d3a107aca94
Jackson LaMotte <kite7742@gmail.com>
2022-01-03 20:21:02 -0800

    Initial commit

---
+++ src/main/java/dev/polychroma/configeditor/Configeditor.java
@@ -0,0 +1,17 @@
+package dev.polychroma.configeditor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Configeditor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
